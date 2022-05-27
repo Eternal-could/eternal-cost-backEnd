ff2d252bb76a00200bff174cde44be64a3a5a599
Eternal-could <20578466@qq.com>
2022-05-27 16:13:57 +0800

    完善注册接口及功能

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// 默认头像
+const defaultAvatar = 'https://p1-bk.byteimg.com/tos-cn-i-mlhdmxsy5m/479e4754b1fe464a9cf49549fa2f802e~tplv-mlhdmxsy5m-q75:0:0.image';
 const Controller = require('egg').Controller;
 
 class UserController extends Controller {
@@ -10,6 +12,39 @@ class UserController extends Controller {
       ctx.body = {
         code: 500,
         msg: '账号密码不能为空',
+        data: null,
+      };
+      return;
+    }
+    // 验证数据库内是否存在同名用户
+    const userInfo = await ctx.service.user.getUserByName(username); // 获取用户信息
+    console.log('userInfo', userInfo);
+    if (userInfo && userInfo.id) {
+      ctx.body = {
+        code: 500,
+        msg: '账户名已被注册， 请重新输入',
+        data: null,
+      };
+      return;
+    }
+    // 注册，将数据存入数据库
+    const result = await ctx.service.user.register({
+      username,
+      password,
+      signature: 'eternal~',
+      avatar: defaultAvatar,
+      ctime: Math.ceil(new Date().getTime() / 1000),
+    });
+    if (result) {
+      ctx.body = {
+        code: 200,
+        msg: '注册成功',
+        data: null,
+      };
+    } else {
+      ctx.body = {
+        code: 500,
+        msg: '注册失败',
         data: null,
       };
     }
