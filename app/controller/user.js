c2d69d2a0a3694d86bd5684e90c61ccbf8dee4d5
Eternal-could <20578466@qq.com>
2022-05-27 16:40:22 +0800

    完善注册接口密码加密

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const crypto = require('crypto');
+const { v4 } = require('uuid');
 // 默认头像
 const defaultAvatar = 'https://p1-bk.byteimg.com/tos-cn-i-mlhdmxsy5m/479e4754b1fe464a9cf49549fa2f802e~tplv-mlhdmxsy5m-q75:0:0.image';
 const Controller = require('egg').Controller;
+
+// 创建一个工具函数 加密
+function enCryptData(data, key, algorithm) {
+  if (!crypto.getHashes().includes(algorithm)) {
+    throw new Error('不支持此哈希函数');
+  }
+  const hmac = crypto.createHmac(algorithm, key);
+  hmac.update(data);
+  return hmac.digest('hex');
+}
 
 class UserController extends Controller {
   async register() {
@@ -28,9 +40,11 @@ class UserController extends Controller {
       return;
     }
     // 注册，将数据存入数据库
+    const key = v4();
+    const enPassword = enCryptData(ctx.request.body.password, key, 'sha256');
     const result = await ctx.service.user.register({
       username,
-      password,
+      password: enPassword,
       signature: 'eternal~',
       avatar: defaultAvatar,
       ctime: Math.ceil(new Date().getTime() / 1000),
