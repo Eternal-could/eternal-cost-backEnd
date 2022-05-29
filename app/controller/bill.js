36a014b7be3c98f8997caa36dac64ea845f3cdc7
Eternal-could <20578466@qq.com>
2022-05-29 22:32:25 +0800

    获取账单详情

--- app/controller/bill.js
+++ app/controller/bill.js
@@ -137,6 +137,41 @@ class BillController extends Controller {
       };
     }
   }
+  // 获取账单详情
+  async detail() {
+    const { ctx, app } = this;
+    const { id = '' } = ctx.query;
+    let user_id;
+    const token = ctx.request.header.authorization;
+    // 获取当前用户信息
+    const decode = await app.jwt.verify(token, app.config.jwt.secret);
+    if (!decode) return;
+    user_id = decode.id;
+    // 判断是否传入账单 id
+    if (!id) {
+      ctx.body = {
+        code: 500,
+        msg: '订单id不能为空',
+        data: null,
+      };
+      return;
+    }
+    try {
+      // 从数据库获取账单详情
+      const detail = await ctx.service.bill.detail(id, user_id)
+      ctx.body = {
+        code: 200,
+        msg: '请求成功',
+        data: detail,
+      };
+    } catch (e) {
+      ctx.body = {
+        code: 500,
+        msg: '系统错误',
+        data: null,
+      };
+    }
+  }
 }
 
 module.exports = BillController;
